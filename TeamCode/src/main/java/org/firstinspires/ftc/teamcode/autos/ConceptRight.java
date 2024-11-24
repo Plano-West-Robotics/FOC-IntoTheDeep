00f559334623e5519c33aa92dede77855fc14c11
Faizan Masroor <faizanmasroor01@gmail.com>
2024-11-23 22:12:09 -0600

    Add ConceptRight.java (Rahul's theoretical plan for an autonomous OpMode).

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/ConceptRight.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+public class ConceptRight {
+    /*
+        Theoretical auto plan (use IMU for maintaining proper orientation):
+        Additional idea: use april tags to make sure the robot is positioned correctly because
+        drive motor encoders can slip
+
+        Score two specimens (one preloaded), one sample, and park
+
+
+        Load with specimen
+
+        Clamp down claw
+        Extend intake arm
+        Move forward slightly
+        Turn 180 degrees - or maybe just position the robot backwards to begin with
+
+        Simultaneously:
+        Strafe left (relative to field)
+        Extend outtake arm
+        Raise linear slide
+
+        Stop the linear slide when it's above the high rung
+        Stop strafing and move forward until the specimen is over the bar
+        Move the linear slide downward until the specimen hooks on
+
+        Open claw and move backward
+        ... to be continued
+
+     */
+}

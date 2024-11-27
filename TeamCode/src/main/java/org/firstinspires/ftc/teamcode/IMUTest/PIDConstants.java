a1430868f1c7fa74ae2d4692f85e238ed37a3b83
askryan <askryan557@gmail.com>
2024-11-26 19:24:04 -0600

    removed unused code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest/PIDConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest/PIDConstants.java
@@ -6,16 +6,18 @@ import com.acmerobotics.dashboard.config.Config;
 public class PIDConstants
 {
     /*
+
     Tuning guide:
 
     Set the I and D gains to zero
     Increase the P gain until there are oscillations around the target
     Increase the D gain until no overshoot occurs
     If there is steady state error, increase the I gain until it is corrected
+
      */
 
     public static double Kp = 2;
     public static double Ki = 0.0;
     public static double Kd = 0.0;
-    public static double targetAngle = 0;
+
 }

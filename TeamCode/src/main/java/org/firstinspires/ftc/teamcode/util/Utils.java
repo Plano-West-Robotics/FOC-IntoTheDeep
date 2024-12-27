36b316259a9b98dc5279c7f33f1a995f9eeefa71
Faizan Masroor <faizanmasroor01@gmail.com>
2024-12-27 13:42:36 -0600

    Document Utils.calculateAllowedPower.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Utils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Utils.java
@@ -4,6 +4,22 @@ public final class Utils
 {
     private Utils() {}
 
+    /**
+     * Calculates the maximum power value that can safely be used in a setPower call for a motor
+     * which drives a linear slide. The graph of this function, where the x-axis is
+     * {@code currentPosition} and the y-axis is the return value, tapers down to
+     * y = {@code minPower} near x = 0 and x = {@code maxPosition}, approaches y = {@code maxPower}
+     * in for x-values in the middle, and uses a reflection at x = 0 and x = {@code maxPosition}
+     * (accomplished with the local double {@code c}) so that the method still returns a reasonable
+     * power value when x is not within the domain [0, {@code maxPosition}].
+     * @param minPower the minimum value the method may return
+     * @param maxPower the maximum value the method may return
+     * @param k a constant that influences the steepness of the mathematical function's graph
+     * @param maxPosition the motor's encoder value at the slide's maximum extension
+     * @param currentPosition the argument of mathematical function
+     * @return the maximum power value that can safely be used given the current position
+     * @throws IllegalArgumentException when any of the preconditions are not met
+     */
     public static double calculateAllowedPower(double minPower, double maxPower, double k, int maxPosition, int currentPosition) throws IllegalArgumentException
     {
         validate(minPower > 0 && minPower < 1, "minPower must be greater than 0 but less than 1.");

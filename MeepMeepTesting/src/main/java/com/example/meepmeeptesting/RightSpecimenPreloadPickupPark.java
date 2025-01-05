bed97d75db88ba758a47106aa3b9e13070cd4216
askryan <askryan557@gmail.com>
2025-01-05 14:43:29 -0600

    Hooks the preloaded specimen, picks up the second one at the edge of the zone, hooks it, then goes back to park

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RightSpecimenPreloadPickupPark.java
@@ -0,0 +1,42 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class RightSpecimenPreloadPickupPark
+{
+    public static double inRad(double degrees)
+    {
+        return Math.toRadians(degrees);
+    }
+
+    public static void main(String[] args)
+    {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, trackWidth
+                .setConstraints(50, 15, inRad(135), inRad(135), 15)
+                .build();
+
+        Pose2d initPose = new Pose2d(24, -61, inRad(90));
+
+        myBot.runAction(myBot.getDrive().actionBuilder(initPose)
+                .splineToConstantHeading(new Vector2d(10, -35), inRad(90))
+                        .waitSeconds(2)
+                .strafeTo(new Vector2d(34, -61))
+                        .waitSeconds(2)
+                .strafeTo(new Vector2d(10, -35))
+                        .waitSeconds(2)
+                .strafeTo(new Vector2d(58, -58))
+                .build()
+        );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .addEntity(myBot)
+                .start();
+    }
+}

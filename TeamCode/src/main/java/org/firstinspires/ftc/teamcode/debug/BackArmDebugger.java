499b5f372be8b77f502c0a312016b73925ecabd8
Faizan Masroor <faizanmasroor01@gmail.com>
2025-01-12 14:11:26 -0600

    Make changes to arm debuggers.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug/BackArmDebugger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug/BackArmDebugger.java
@@ -3,7 +3,9 @@ package org.firstinspires.ftc.teamcode.debug;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.teamcode.hardware.outtake.BackArm;
 import org.firstinspires.ftc.teamcode.wrappers.ServoPair;
 
 @Config
@@ -13,7 +15,7 @@ public class BackArmDebugger extends OpMode
     public static double leftPosition;
 
     public ServoPair arm;
-    public final double positionDiff = 0.08;
+    public final double positionDiff = BackArm.POSITION_DIFF;
 
     @Override
     public void init()
@@ -24,7 +26,7 @@ public class BackArmDebugger extends OpMode
     @Override
     public void loop()
     {
-        if (!(leftPosition >= positionDiff && leftPosition <= 1)) leftPosition = positionDiff;
+        leftPosition = Range.clip(leftPosition, positionDiff, 1);
         arm.setPosition(leftPosition);
     }
 }

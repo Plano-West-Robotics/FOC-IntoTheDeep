e457730ba0a3042036c2a7a3ff7e7228b73fde2f
Faizan Masroor <faizanmasroor01@gmail.com>
2025-01-21 18:53:19 -0600

    Update vertical gravity feedforward.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/outtake/VerticalExtendo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/outtake/VerticalExtendo.java
@@ -30,7 +30,7 @@ public class VerticalExtendo extends Extendo
                 0.0025,
                 P, I, D, F,
                 5,
-                0.00025
+                0.0005
         );
         getLeft().reverse();
         getRight().forward();
